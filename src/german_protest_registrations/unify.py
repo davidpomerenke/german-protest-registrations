7b065ac021d5a1d46f311006ec9a3913c9a49f55
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2023-11-03 18:04:32 +0100

    refactor(unify.py): improve

--- src/german_protest_registrations/unify.py
+++ src/german_protest_registrations/unify.py
@@ -3,9 +3,9 @@ import warnings
 
 import pandas as pd
 from dateparser import parse
-import swifter
-
+from joblib import Memory
 from tqdm import tqdm
+
 from german_protest_registrations.readers.augsburg import augsburg
 from german_protest_registrations.readers.berlin import berlin
 from german_protest_registrations.readers.bremen import bremen
@@ -26,8 +26,37 @@ from german_protest_registrations.readers.saarbruecken import saarbruecken
 from german_protest_registrations.readers.wiesbaden import wiesbaden
 
 warnings.filterwarnings("ignore", module="dateparser")
+cache = Memory(".cache", verbose=0).cache
 
 
+@cache
+def get_df():
+    df_readers = [
+        augsburg,
+        berlin,
+        bremen,
+        dortmund,
+        dresden,
+        duisburg,
+        erfurt,
+        freiburg,
+        karlsruhe,
+        kiel,
+        koeln,
+        magdeburg,
+        mainz,
+        mannheim,
+        muenchen,
+        potsdam,
+        saarbruecken,
+        wiesbaden,
+    ]
+    dfs = {read.__name__: read() for read in tqdm(df_readers, mininterval=1)}
+    df = pd.concat(dfs, ignore_index=True)
+    return df
+
+
+@cache
 def parse_dates(df):
     """Automatically parse dates from the event_date column.
     On the original data set, `dateparser` fails to parse 656 of 54246 dates, and the additional rules reduce this to 82.
@@ -57,9 +86,12 @@ def parse_dates(df):
         )
     )
     print(f"Could not parse {df['event_date'].isnull().sum()} of {len(df)} dates")
+    return df
+
+
+def add_unparsable_dates(df):
     # write the original texts of the unparsable dates to a json file
     keys = df["event_date_text"][df["event_date"].isnull()].tolist()
-    print(keys)
     file = "data/interim/unparsable_dates.json"
     if False:
         with open(file, "w") as f:
@@ -68,48 +100,49 @@ def parse_dates(df):
         # assume that a human has entered the correct dates as values into the json file and read them back in
         with open(file) as f:
             unparseable_dates = json.load(f)
-        df.loc[df["event_date"].isnull(), "event_date"] = (
-            pd.Series(unparseable_dates).astype(str).apply(parse).values
-        )
-    df["event_date"] = df["event_date"].dt.date
+        unparseable_dates = pd.Series(unparseable_dates).astype(str).apply(parse).values
+        df.loc[df["event_date"].isnull(), "event_date"].update(unparseable_dates)
+        df = df[~df["event_date"].isnull()]
     return df
 
 
 def main():
-    df_readers = [
-        augsburg,
-        berlin,
-        bremen,
-        dortmund,
-        dresden,
-        duisburg,
-        erfurt,
-        freiburg,
-        karlsruhe,
-        kiel,
-        koeln,
-        magdeburg,
-        mainz,
-        mannheim,
-        muenchen,
-        potsdam,
-        saarbruecken,
-        wiesbaden,
-    ]
-    dfs = {read.__name__: read() for read in tqdm(df_readers, mininterval=1)}
-    df = pd.concat(dfs, ignore_index=True)
+    df = get_df()
     df = parse_dates(df)
+    df = add_unparsable_dates(df)
+    df["event_date"] = df["event_date"].dt.date
+    df = df.drop(columns=["event_date_text"])
+    # event details are missing before these dates
+    df = df[
+        ~((df["city"] == "Kiel") & (df["event_date"] < pd.to_datetime("2021-04-01").date()))
+        & ~(
+            (df["city"] == "Saarbrücken") & (df["event_date"] < pd.to_datetime("2021-06-01").date())
+        )
+        & ~((df["city"] == "Freiburg") & (df["event_date"] < pd.to_datetime("2013-01-01").date()))
+        & ~((df["city"] == "Bremen") & (df["event_date"] < pd.to_datetime("2019-01-01").date()))
+        & ~((df["city"] == "Dresden") & (df["event_date"] < pd.to_datetime("2020-07-01").date()))
+        & ~((df["city"] == "Köln") & (df["event_date"] < pd.to_datetime("2018-01-01").date()))
+        & ~((df["city"] == "Magdeburg") & (df["event_date"] < pd.to_datetime("2015-01-01").date()))
+    ]
+    # remove weird dates
+    df = df[
+        (df["event_date"] < pd.to_datetime("2023-01-01").date())
+        & (df["event_date"] >= pd.to_datetime("2010-01-01").date())
+    ]
+    df = df.sort_values(["city", "event_date"])
+    df = df[
+        [
+            "city",
+            "event_date",
+            "organizer",
+            "topic",
+            "participants_registered",
+            "participants_actual",
+        ]
+    ]
     df.to_csv("data/processed/german_protest_registrations.csv", index=False)
     print(df.shape)
 
 
 if __name__ == "__main__":
     main()
-
-
-# TODO: date limits in Kiel and Saarbrücken
-# only keep records after 2021-05-21, prior records contain no topics
-# df = df[df["event_date"] >= "2021-05-21"]
-#     df = df[
-#     df["event_date"] >= pd.to_datetime("2021-03-26").date()
-# ]  # event details are only available from this date
