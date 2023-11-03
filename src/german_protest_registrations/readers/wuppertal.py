c96de2534918f35e8f8edd26785eaaabc866eeb4
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2023-11-03 18:02:58 +0100

    feat(data): add Wuppertal

---
+++ src/german_protest_registrations/readers/wuppertal.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+import pandas as pd
+
+
+def wuppertal():
+    path = Path("data/interim/csv/Wuppertal")
+    dfs = []
+    for file in path.glob("*.csv"):
+        df = pd.read_csv(file, skiprows=2)
+        df = df.rename(
+            columns={
+                "Datum": "event_date",
+                "Veranstalter": "organizer",
+                "Thema": "topic",
+                "Versammlungsort": "location",
+                "Teilnehmer": "participants_registered",
+            },
+        )
+        df = df.dropna(subset=["event_date"])
+        dfs.append(df)
+    df = pd.concat(dfs)
+    df = df[["event_date", "organizer", "topic", "location", "participants_registered"]]
+    df["city"] = "Wiesbaden"
+    return df
+
+
+if __name__ == "__main__":
+    df = wuppertal()
+    print(df.sample(20))
